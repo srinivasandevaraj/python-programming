6f0e3b1d3a8f8c513043fbee587f24603708b7bc
srinivasandevaraj <37359322+srinivasandevaraj@users.noreply.github.com>
2018-03-21 06:24:19 +0000

    Update character.py

--- Beginner/character.py
+++ Beginner/character.py
@@ -1,5 +1,5 @@
 ch=input()
-if(ch=='a'or ch=='e'or ch=='i'or ch=='o'or ch=='u'):
+if(ch=='a'or ch=='e'or ch=='i'or ch=='o'or ch=='u') or (ch=='A' or ch=='E' or ch=='I' or ch=='O' or ch=='U'):
     print("Vowels")
 else:
     print("Consonant")
