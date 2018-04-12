5b3ba0675baf7b87fe5fedf270cdabe57543b640
srinivasandevaraj <37359322+srinivasandevaraj@users.noreply.github.com>
2018-04-12 17:50:12 +0530

    Update leap year.py

--- Beginner/leap year.py
+++ Beginner/leap year.py
@@ -1,10 +1,10 @@
 a=int(input("Enter the year:"))
 if(a%100==0):
   if(a%400==0):
-    print("Yes")
+    print("yes")
   else:
-    print("No")
+    print("no")
 elif(a%4==0):
-  print("Yes")
+  print("yes")
 else:
   print("no")
