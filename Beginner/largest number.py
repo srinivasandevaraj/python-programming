d6fb25d5e7add2ef1e95971a09bda5643f985ec6
srinivasandevaraj <37359322+srinivasandevaraj@users.noreply.github.com>
2018-04-12 17:29:47 +0530

    Create largest number.py

---
+++ Beginner/largest number.py
@@ -0,0 +1,11 @@
+a=int(input())
+b=int(input())
+c=int(input())
+if(a>b)and(a>c):
+  print("a is greather")
+elif(b>a)and(b>c):
+  print("b is greather")
+elif(c>a)and(b>a):
+  print("c is greather")
+else:
+  print("invalid")
